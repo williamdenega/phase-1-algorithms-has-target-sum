3e72604d1acbb4654452b73118f53b29dcc595ec
WilliamDenega <williamdenega@gmail.com>
2022-01-31 16:06:58 -0500

    commit tree 87735a83be52edece6b66b2bf7562711a21b489a

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let checkSum = 0
+  for(let i = 0; i<array.length;i++){
+    for(let j=i+1; j<array.length;j++){
+      let sum = array[i] + array[j]
+      if(sum === target){
+        checkSum = 1
+      }
+
+    }
+
+
+  }
+  if(checkSum === 1){
+    return true
+  }else{
+    return false
+  }
+  
+
+
 }
+
+
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
+
+  It is nested for loops so it is = O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  
+  loop through each element of the array
+  add 2 elements together and check if they equal the sum
+  return true if two numbers in the array add up to the sum
+
+
 */
 
 /*
   Add written explanation of your solution here
+
+  What I did was loop through the array one element at a time. 
+  Then I would check the sum of each element added together
+  If they did, I set a variable to 1 and after it iterates through the
+  whole array, I would check and see if was set to 1, if yes I would return true
+
+
 */
 
 // You can run `node index.js` to view these console logs
